14f756c6857cd060c54106ddf218cf767db44034
nmashimb <50702463+nmashimb@users.noreply.github.com>
2020-07-10 20:40:59 +0200

    Add files via upload

---
+++ public/scripts/script.js
@@ -0,0 +1,19 @@
+const socket = io('http://localhost:3001');
+
+socket.on('chat', data => {
+    console
+})
+
+
+
+/*
+function myFunction()
+{
+    var checkbox = document.getElementById("myCheck");
+    var text = document.getElementById("text");
+    if (checkbox.checked == true){
+        text.style.display = "none";
+    }
+        else
+        text.style.display = "block";
+}*/
