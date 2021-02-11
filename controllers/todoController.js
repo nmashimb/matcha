f2df55f7b6536fd556b131090165860446b2c733
unknown <nmashimb>
2021-02-11 16:40:14 +0200

    done

--- controllers/todoController.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app){
-
-    app.get('/todo', function(req, res){
-
-    });
-    app.post('./todo', function(req, res){
-
-    });
-};
--- serve.js
+++
@@ -1,6 +0,0 @@
-const io = require('socket.io')(3001);
-
-io.on('connection', socket =>  {
-  console.log('connected user');
-  socket.emit('chat-message','hello world')
-})
